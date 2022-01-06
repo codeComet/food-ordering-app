92f010e3b448add59348d453e2521792699e44d1
codeComet <unknownmind10@gmail.com>
2022-01-06 23:39:07 +0600

    nav bar completed

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -5,24 +5,39 @@ import { BsTelephoneOutbound } from "react-icons/bs";
 const Navbar = () => {
   return (
     <div className={styles.container}>
+      <>
+        <div className={styles.nav}>
+          <div className={styles.num_container}>
+            <BsTelephoneOutbound
+              style={{ margin: "5px 5px 0px 0px", color: "#fff" }}
+            />{" "}
+            <span className={styles.number}>123 456 665</span>
+            <span className={styles.number}> To order</span>
+          </div>
+          <div className={styles.navitem}>
+            <a href="#">Specials</a>
+          </div>
+          <div className={styles.navitem}>
+            <a href="#">Menu</a>
+          </div>
+        </div>
+      </>
       <div className={styles.logo}>
         <a href="/">Foodify</a>
       </div>
-      <div className={styles.nav}>
-        <div>
-          <BsTelephoneOutbound
-            style={{ margin: "5px 5px 0px 0px", color: "#fff" }}
-          />{" "}
-          <span className={styles.number}>123 456 665</span>
-          <span className={styles.number}> To order</span>
+      <>
+        <div className={styles.nav}>
+          <div className={styles.navitem}>
+            <a href="#">Products</a>
+          </div>
+          <div className={styles.navitem}>
+            <a href="#">Blog</a>
+          </div>
+          <div className={styles.navitem}>
+            <a href="#">Contact us</a>
+          </div>
         </div>
-        <div className={styles.navitem}>
-          <a href="#">Specials</a>
-        </div>
-        <div className={styles.navitem}>
-          <a href="#">Contact us</a>
-        </div>
-      </div>
+      </>
     </div>
   );
 };
