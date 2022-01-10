4000d199e6499570bbccf899d8fc7f1d947dd799
codeComet <unknownmind10@gmail.com>
2022-01-10 22:55:37 +0600

    single product page design

--- client/pages/item/[id].jsx
+++ client/pages/item/[id].jsx
@@ -7,6 +7,14 @@ const FoodItem = () => {
       <div className={styles.imgContainer}>
         <Image src="/img/1.jpg" alt="food" width={300} height={300} />
       </div>
+      <div className={styles.infoContainer}>
+        <h1>Food Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+          numquam?
+        </p>
+        <p>$10.00</p>
+      </div>
     </div>
   );
 };
