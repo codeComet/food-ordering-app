71c5bce477674a89588a583a159ce671fa951760
codeComet <unknownmind10@gmail.com>
2022-01-10 22:58:16 +0600

    single product page design needs some work

--- client/pages/item/[id].jsx
+++ client/pages/item/[id].jsx
@@ -8,16 +8,18 @@ const FoodItem = () => {
         <Image src="/img/1.jpg" alt="food" width={300} height={300} />
       </div>
       <div className={styles.infoContainer}>
-        <h1>Food Name</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-          numquam?
-        </p>
-        <p>$10.00</p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <input type="number" />
-        <button>Add to Cart</button>
+        <div className={styles.foodInfo}>
+          <h1>Food Name</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            numquam?
+          </p>
+          <p>$10.00</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <input type="number" />
+          <button>Add to Cart</button>
+        </div>
       </div>
     </div>
   );
