c1b268f1f59d3175da371f6cfd6361e4d8b01ed2
codeComet <unknownmind10@gmail.com>
2022-01-20 11:59:12 +0600

    special product section completed with slider and responsiveness

--- client/components/Special.jsx
+++ client/components/Special.jsx
@@ -1,6 +1,11 @@
 import styles from "../styles/Special.module.css";
 import Item from "./Item";
-import { Carousel } from "@trendyol-js/react-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css";
+
+SwiperCore.use([Pagination]);
 
 const Special = () => {
   return (
@@ -14,33 +19,68 @@ const Special = () => {
         </p>
       </div>
       <div className={styles.special_container}>
-        <Carousel
-          show={4.5}
-          slide={1}
-          swiping={true}
-          transition={1}
-          responsive={true}
-          className={styles.slider}
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{ clickable: true }}
+          j
+          onSwiper={(swiper) => console.log(swiper)}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2.5,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3.5,
+              spaceBetween: 50,
+            },
+            1440: {
+              slidesPerView: 4.5,
+              spaceBetween: 60,
+            },
+          }}
+          className={styles.swiper}
         >
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-          <div className={styles.special_item}>
-            <Item />
-          </div>
-        </Carousel>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.special_item}>
+              <Item />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.soon_container}>
+              <p>More are coming soon</p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
